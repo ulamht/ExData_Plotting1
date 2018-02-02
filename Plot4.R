a684d93cf90136cacdaf5af03719b935f8802612
Hoang Lam <33915376+ulamht@users.noreply.github.com>
2018-02-01 22:26:05 -0800

    Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -1,5 +1,16 @@
+power <- read.table("C:\\Users\\tuuye\\Downloads\\household_power_consumption.txt", stringsAsFactors=FALSE, header=TRUE, sep=";")
+FullTimeDate <- strptime(paste(power$Date, power$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+power <- cbind(power, FullTimeDate)
+
+power$Date <- as.Date(power$Date, format="%d/%m/%Y")
+power$Time <- format(power$Time, format="%H:%M:%S")
+power$Sub_metering_1 <- as.numeric(power$Sub_metering_1)
+power$Sub_metering_2 <- as.numeric(power$Sub_metering_2)
 power$Sub_metering_3 <- as.numeric(power$Sub_metering_3)
+
+
 subsetdata <- subset(power, Date == "2007-02-01" | Date =="2007-02-02")
+
 png("plot4.png", width=480, height=480)
 par(mfrow=c(2,2))
 with(subsetdata, plot(FullTimeDate, Global_active_power, type="l", xlab="", ylab="Global Active Power"))
