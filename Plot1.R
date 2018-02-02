0b83da71ce5cc3040bdf1bd70686777b9483d8e1
Hoang Lam <33915376+ulamht@users.noreply.github.com>
2018-02-01 21:50:31 -0800

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -4,4 +4,4 @@ subset <- subset(power, Date %in% c("1/2/2007", "2/2/2007"))
 subset$Date <- as.Date(subset$Date, format="%d/%m/%y")
 png("plot1", width=480, height=480)
 hist(as.numeric(as.character(subset$Global_active_power)), main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="red")
-
+dev.off()
