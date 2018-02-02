2ea159537ea81a16602edb294e198b6842722853
Hoang Lam <33915376+ulamht@users.noreply.github.com>
2018-02-01 22:09:29 -0800

    Plot3.R

---
+++ Plot3.R
@@ -0,0 +1,8 @@
+power$Sub_metering_3 <- as.numeric(power$Sub_metering_3)
+subsetdata <- subset(power, Date == "2007-02-01" | Date =="2007-02-02")
+png("plot3.png", width=480, height=480)
+with(subsetdata, plot(FullTimeDate, Sub_metering_1, type="l", xlab="Day", ylab="Energy sub metering"))
+lines(subsetdata$FullTimeDate, subsetdata$Sub_metering_2,type="l", col= "red")
+lines(subsetdata$FullTimeDate, subsetdata$Sub_metering_3,type="l", col= "blue")
+legend(c("topright"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty= 1, lwd=2, col = c("black", "red", "blue"))
+dev.off()
